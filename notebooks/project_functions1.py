4249e5bc80e0877ea9d4ae48c7c0ee6a6c28f48b
Zecr <z.edwin@outlook.com>
2022-11-05 16:51:38 -0700

    Finished data ingest pipeline

--- notebooks/project_functions1.py
+++ notebooks/project_functions1.py
@@ -10,8 +10,8 @@ def dataloadnclean(path):
         [dataset['types'] != 'bundle']
         .drop(columns = ['url', 'game_description', 'minimum_requirements', 'recommended_requirements', 'desc_snippet', 'mature_content', 'types'])
         [dataset['all_reviews'].notna() & dataset['all_reviews'].str.contains('%')]
-        .assign(all_reviews = lambda x: x['all_reviews'].apply(lambda y: re.findall("(\d{1,3}%)", y)[0]))
+        .assign(all_reviews = lambda x: x['all_reviews'].apply(lambda y: re.findall("(\d{1,3}%)", y)[0].replace('%', '').to_int()))
         [dataset['recent_reviews'].notna() & dataset['recent_reviews'].str.contains('%')]
-        .assign(recent_reviews = lambda x: x['recent_reviews'].apply(lambda y: re.findall("(\d{1,3}%)", y)[0]))
+        .assign(recent_reviews = lambda x: x['recent_reviews'].apply(lambda y: re.findall("(\d{1,3}%)", y)[0].replace('%', '').to_int()))
     )
     return dataset
