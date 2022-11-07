5f09f1aef6ab87fbaec389e5996299f18017f673
Zecr <z.edwin@outlook.com>
2022-11-06 16:43:58 -0800

    Added to analysis section

--- notebooks/project_functions1.py
+++ notebooks/project_functions1.py
@@ -8,10 +8,12 @@ def dataloadnclean(path):
     dataset = (
         dataset
         [dataset['types'] != 'bundle']
-        .drop(columns = ['url', 'game_description', 'minimum_requirements', 'recommended_requirements', 'desc_snippet', 'mature_content', 'types'])
+        .drop(columns = ['url', 'game_description', 'minimum_requirements', 'recommended_requirements', 'desc_snippet', 'mature_content', 'types', 'discount_price'])
         [dataset['all_reviews'].notna() & dataset['all_reviews'].str.contains('%')]
-        .assign(all_reviews = lambda x: x['all_reviews'].apply(lambda y: re.findall("(\d{1,3}%)", y)[0].replace('%', '').to_int()))
+        .assign(all_reviews = lambda x: x['all_reviews'].apply(lambda y: int(re.findall("(\d{1,3}%)", y)[0].replace('%', ''))))
         [dataset['recent_reviews'].notna() & dataset['recent_reviews'].str.contains('%')]
-        .assign(recent_reviews = lambda x: x['recent_reviews'].apply(lambda y: re.findall("(\d{1,3}%)", y)[0].replace('%', '').to_int()))
+        .assign(recent_reviews = lambda x: x['recent_reviews'].apply(lambda y: int(re.findall("(\d{1,3}%)", y)[0].replace('%', ''))))
+        [dataset['original_price'].notna() & (dataset['original_price'].str.contains('\$') | dataset['original_price'].str.contains('free', case=False))]
+        .assign(original_price = lambda x: x['original_price'].apply(lambda y: 0 if 'free' in y.lower() else float(y.replace('$', ''))))
     )
     return dataset
